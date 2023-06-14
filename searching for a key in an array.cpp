f3bb382ee6f254899c2f3c20547e3fda41426665
nagmaSiddiqui <106270858+nagmaSiddiqui@users.noreply.github.com>
2023-06-15 01:07:32 +0530

    Update searching for a key in an array.cpp

--- searching for a key in an array.cpp
+++ searching for a key in an array.cpp
@@ -4,6 +4,12 @@
 #include<iostream>
 using namespace std;
  bool search(int arr[],int size, int key){
+  for (int i=0; i<size; i++){
+     if (arr[i]==key){
+      return 1;
+       }
+  }
+     return 0;
      
  }
  
